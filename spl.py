10930c7bad9b3c0821e93f1d6113e0350364878b
hellking4u <hellking4u@gmail.com>
2016-04-22 15:40:09 -0400

    added some comments and made things more sane

--- spl.py
+++ spl.py
@@ -1,9 +1,13 @@
-# from scipy.io.wavfile import read
-# samprate, wavdata = read('mitra_1.wav')
+'''
+SPL.py
+
+Calculate Sound Pressure Levels
+
+'''
+import numpy as np
+from math import log10, sqrt
 
 import wavio
-from math import log10, sqrt
-from data import concourse_2_60, train_1_2_3_day_1_60, train_1_2_3_day_2_60, weekday_day1
 
 def plot_data(y, factor=2, interpolate=True):
 	x = range(len(y))
@@ -29,56 +33,48 @@ def plot_data(y, factor=2, interpolate=True):
 	return None
 
 def getdecibels(filename, chunks=None, chunk_factor=1):
+	'''
+	getdecibels function takes a filename and produces the decibel readings from it.
+
+	TODO : It returns a list right now, it would be nice if it could be converted into a generator
+
+	Arguments :
+	chunks: default None. The number of chunks to divide the audio file into.
+
+	chunk_factor : If chunks is not provided, we split the audio files according to the 
+					chunk_factor. This essentially refers to number of chunks per minute.
+					If the total duration is less than a minute, we will take 2 chunks.
+	'''
+
+	#get data from the wavio.
+	#NOTE : The original wavio module does not return the duration.
 	duration, samprate, sampwidth, wavdata = wavio.readwav(filename)
 
 
+	#figure out exactly how many chunks we need to split it.
 	if chunks==None:
 		chunks = max(int(duration/60),2)*chunk_factor
+
+
+	#we need to convert the wavdata into 64 bits because calculating the decibels require squaring it
+	# If it's stored as a 32 bit int, the integer will overflow.
 	wavdata = wavdata.astype('int64')
-	import numpy as np
-	numchunks = chunks
-	chunks = np.array_split(wavdata, numchunks)
-	wavdata = 0 # clear out about a gig of data
-	# print chunks**2
-	# a = chunks[0]
-	# b = np.square(a,dtype='int64')
-	# print a.dtype
-	# for i,j in zip(a,b):
-	# 	print i,j, i**2
-	dbs = [np.mean(chunk**2) for chunk in chunks]
-	print "section 1"
+
+	#Split the dataset into chunks
+	chunks = np.array_split(wavdata, chunks)
+
+	# clear out about a gig of data. This simple line saved a ton of trouble while dealing with large files.
+	wavdata = 0 
+
+	#dB SPL is basically dB = 20 * log10(amplitude)
 	dbs = [20*log10( sqrt(np.mean(chunk**2)) ) for chunk in chunks]
-	print "done dbs calculation"
+
 	return dbs
 
-def fan_experiment():
-	x = [0,2,4,6,8,10]
-	y = []
-	for i in x:
-		fname = 'audio_data/fan/{0}.WAV'.format(i)
-		print fname
-		data = getdecibels(fname, 1)
-		y.append(data[0])
-		# print i
-	# print getdecibels('fan/0.WAV', 1)
-	# print getdecibels('fan/2.WAV', 1)
-	# print getdecibels('fan/4.WAV', 1)
-	# print getdecibels('fan/6.WAV', 1)
-	# print getdecibels('fan/8.WAV', 1)
-	# print getdecibels('fan/10.WAV', 1)
-
-	import matplotlib.pyplot as plt
-	from scipy.interpolate import interp1d
-	import numpy as np
-	xnew = np.linspace(0, 10, num=41, endpoint=True)
-	f = interp1d(x, y)
-	f2 = interp1d(x, y, kind='cubic')
-	plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
-	plt.legend(['data', 'linear', 'cubic'], loc='best')
-	# plt.plot(,y)
-	plt.show()
-
 if __name__ == '__main__':
+	# Uncomment the next line to use precalculated values.
+	#from data import concourse_2_60, train_1_2_3_day_1_60, train_1_2_3_day_2_60, weekday_day1
+
 	dbs = getdecibels('/Users/sag47/Downloads/Cecilia.WAV', chunk_factor=60)
 	print dbs
 	# plot_data(weekday_day1, factor=5, interpolate=True)
