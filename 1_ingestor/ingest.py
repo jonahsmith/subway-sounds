07637f0cef1d59950255f0e6e11b0a5ab160bc93
jonahsmith <smith.jonah@gmail.com>
2016-04-25 19:17:39 -0400

    adding redis specifics

--- 1_ingestor/ingest.py
+++ 1_ingestor/ingest.py
@@ -1,13 +1,20 @@
 import requests
 import json
 from sys import stdout
-from datetime import datetime 
+from datetime import datetime
+import redis
 
 # The URL for the long-lived HTTP stream of sound intensity readings
 URL = 'http://localhost:6998'
 
-# Establish redis connections, one for each level.
+# The amount of time for which values should persist, in seconds
+TTL = 15*60
 
+# Establish redis connections, one table for each level.
+street = redis.Redis(db=0)
+s = redis.Redis(db=1)
+red = redis.Redis(db=2)
+purple = redis.Redis(db=3)
 
 # Establish a connection to the long-lived HTTP stream
 r = requests.get(URL, stream=True)
@@ -16,5 +23,10 @@ r = requests.get(URL, stream=True)
 for line in r.iter_lines(chunk_size=10):
     if line:
         data = json.loads(line)
-        print data, datetime.now()
-        stdout.flush()
+        timestamp = data['time']
+
+        # Add the new readings to the Redis database
+        street.setex(timestamp, data['readings']['street'], TTL)
+        s.setex(timestamp, data['readings']['s'], TTL)
+        red.setex(timestamp, data['readings']['1-2-3'], TTL)
+        purple.setex(timestamp, data['readings']['7'], TTL)
