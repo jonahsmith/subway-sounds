ddb468f81ebb94538b16b801212ea1eaab583c94
jonahsmith <smith.jonah@gmail.com>
2016-04-25 19:17:21 -0400

    changing format of JSON data from playback

--- 0_streaming_server/playback.py
+++ 0_streaming_server/playback.py
@@ -2,13 +2,17 @@ import time
 from sys import stdout
 import json
 from random import random
+import time
 
 while 1:
     print(json.dumps( {
-                        'street': int(random()*100),
-                        's': int(random()*1000),
-                        '1-2-3': int(random()*1000),
-                        '7': int(random()*1000)
+                        'time': int(time.time()),
+                        'readings': {
+                                        'street': int(random()*100),
+                                        's': int(random()*1000),
+                                        '1-2-3': int(random()*1000),
+                                        '7': int(random()*1000)
+                                    }
                       }
                     )
           )
