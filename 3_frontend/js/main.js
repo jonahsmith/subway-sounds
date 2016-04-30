70f42d9c3ad4644c102e2a3b99bc155b47abfd65
anothercookiecrumbles <priyanjana@mac.com>
2016-04-30 11:14:59 -0400

    Tweaks to the front-end.

--- 3_frontend/js/main.js
+++ 3_frontend/js/main.js
@@ -1,7 +1,7 @@
 (function(){
 
   var parallax = document.querySelectorAll(".parallax"),
-    speed = 0.7;
+    speed = 0.5;
 
   window.onscroll = function(){
   [].slice.call(parallax).forEach(function(el,i){
