097cc7818c222c6cc032e9294314e7b737bc359a
jonahsmith <smith.jonah@gmail.com>
2016-05-01 14:43:12 -0400

    fixing small bugs and adding comments

--- 2_view_server/trains.py
+++ 2_view_server/trains.py
@@ -1,28 +1,63 @@
+# trains.py
+# Subway Sounds, written for Storytelling with Streaming Data, Columbia University
+# May 2016
+#
+# This script contains a function, get_schedule(), that returns a dictionary of
+# the next arrival and next departure times, in UTC, for the 1-2-3 platform and
+# the S platform at the Times Square station in New York City. It uses the MTA's
+# live GTFS feed.
+#
+# This script is not intended to be run on its own, but rather to be imported.
+# It is currently imported by viewserver.py.
+
 import requests
 from google.transit import gtfs_realtime_pb2 as gtfs
 
+# Grab the latest feed from the MTA
 r = requests.get('http://datamine.mta.info/mta_esi.php?key=244af9c1c430895a314bcd995a0ba11c&feed_id=1')
+# Initialize a feed object
 feed = gtfs.FeedMessage()
+# And load the feed object from the binary response
 feed.ParseFromString(r.content)
 
 
+# These are all of the code names for Times Square station, identified using the
+# GTFS schedule data from the MTA (http://web.mta.info/developers/developer-data-terms.html)
 times_square = ['127','127N', '127S', '725','725N','725S','902','902N','902S','R16','R16N','R16S']
+# These are the train identifiers we are interested in. GS is the shuttle.
 trains = ['1', '2', '3', 'GS']
 
+# Initialize dictionaries to contain all of the arrival/departure times as an
+# array for each route.
 departures = {train: [] for train in trains}
 arrivals = {train: [] for train in trains}
 
 def get_schedule():
+    """Return next arrival and departure time at 1-2-3 and S platforms, Times Square"""
+    # Loop through the updates...
     for entity in feed.entity:
+        # Make sure the update has actual update objects in it
         if entity.HasField('trip_update'):
+            # For each update...
             for update in entity.trip_update.stop_time_update:
                 route = entity.trip_update.trip.route_id
+                # Check that: 1) the referenced stop is at Times Square, and 2)
+                # the reference route is one of the ones we are interested in.
                 if update.stop_id in times_square and route in trains:
+                    # Some have arrival times, some have departure times; if it
+                    # has one or both, add it to that route's entry in the
+                    # arrival or departure dictionaries.
                     if update.HasField('arrival'):
                         arrivals[route].append(update.arrival.time)
                     if update.HasField('departure'):
                         departures[route].append(update.departure.time)
 
+    # All of the following are in try blocks because min() returns a ValueError
+    # when it receives an empty array. We'll catch that and just set the value
+    # to None.
+    #
+    # They all work the same way. The timestamps are Unix epoch, so we can just
+    # take the minimum to get the next scheduled one.
     try:
         soonest_departure_123 = min(departures['1'] + departures['2'] + departures['3'])
     except ValueError:
@@ -43,6 +78,9 @@ def get_schedule():
     except ValueError:
         soonest_arrival_s = None
 
+    # Generate the final data to return. A simple dictionary structure, with top
+    # level keys corresponding to platform, and the next level of keys
+    # corresponding to the next_arrival and next_departure.
     data = { '1-2-3': {
                         'next_arrival': soonest_arrival_123,
                         'next_departure': soonest_departure_123
--- 2_view_server/viewserver.py
+++ 2_view_server/viewserver.py
@@ -1,46 +1,77 @@
-
+# viewserver.py
+# Subway Sounds, written for Storytelling with Streaming Data, Columbia University
+# May 2016
+#
+# This script queries the Redis database to compute statistics on the most
+# recent data, gets the train schedule from trains.py, and sends the data as a
+# JSON string to stdout, where it is to be picked up by websocketd and broadcast
+# over a websocket. This is done every PAUSE seconds.
 
 import redis
 import json
 import time
 from datetime import datetime, timedelta, date
 from sys import stdout
+# This is our module, also in this directory, that returns the next arrival and
+# departure for a particular platform.
 import trains
 
 
+# This is a global configuration variable that specifies the number of seconds
+# between updates. (In concrete terms, this will specify how often the data on
+# the client's page updates.)
 PAUSE = 5
 
 def create_history(date):
+    """A factory function for historical data containers"""
     history = {
                 'max': {level: make_reading((None, 0)) for level in ['street', 's', 'red', 'purple']},
-                'min': {level: make_reading((None, 999)) for level in ['street', 's', 'red', 'purple']},
+                'min': {level: make_reading((None, int('inf'))) for level in ['street', 's', 'red', 'purple']},
                 'date': date
               }
     return history
 
 
 def make_reading(val):
+    """Given a tuple, return a dictionary in the format of 'measurement' nodes."""
     return {'time': val[0], 'value': val[1]}
 
 
 def get_values(database):
+    """Return (key, value) tuples for every entry in the given Redis database"""
     keys = database.keys()
     values = database.mget(keys)
+    # This removes entries that expired between retrieving the keys and
+    # retrieving the values. This is one option for dealing with the sequential
+    # retrieval of keys and values. Given the rate of the stream, this should
+    # not be more than one entry, if any, meaning it has minimal impact. It also
+    # does not affect the story much, as it just means that an older reading has
+    # expired.
     key_val = [(int(key), float(val)) for key, val in zip(keys, values) if val]
     return key_val
 
 
 def get_stats(level_name, values):
+    """Calculate the min/max over three time scales: Redis TTL, today, yesterday"""
+    # Given all of the values from Redis, we find the min/max. In practical
+    # terms, that means the minimum over the TTL for the database.
     now_min = min(values, key=lambda kv: kv[1])
     now_max = max(values, key=lambda kv: kv[1])
+    # This is the most recent reading. We can do this because the timestamps are
+    # epoch timestamps, e.g. we can just sort for the largest to get the most
+    # recent.
     most_recent = max(values, key=lambda kv: kv[0])
 
+    # Update today's max and min, if the current values are more extreme than
+    # the previously most extreme.
     if today['max'][level_name]['value'] < now_max[1]:
         today['max'][level_name] = make_reading(now_max)
 
     if today['min'][level_name]['value'] > now_min[1]:
         today['min'][level_name] = make_reading(now_min)
 
+    # Construct our data structure using the statistics we've just computed.
+    # This is the schema used by the front end.
     data = {
             'current': make_reading(most_recent),
             'current_peak': make_reading(now_max),
@@ -54,12 +85,8 @@ def get_stats(level_name, values):
     return data
 
 
-
-def get_trains():
-    return trains.get_schedule()
-
-
 def global_setup():
+    """Initialize global data structures (Redis connections and history)"""
     # Establish the historical data dictionaries
     global today, yesterday
     today = create_history(date.today())
@@ -73,35 +100,48 @@ def global_setup():
     purple = redis.Redis(db=3)
 
 
-def main():
+def send():
+    """Send statistics via stdout every PAUSE seconds"""
+    # Repeat indefinitely
     while 1:
         global today, yesterday
 
+        # If the date in the 'today' history log is not actually today, that
+        # means it's just past midnight. As such, we need to move today's into
+        # yesterday, and reset today's.
         if today['date'] != date.today():
             yesterday = today
-            reset_history(today, date.today())
+            today = reset_history(date.today())
 
+        # Calculate the statistics for each level
         street_stats = get_stats('street', get_values(street))
         s_stats = get_stats('s', get_values(s))
         red_stats = get_stats('red', get_values(red))
         purple_stats = get_stats('purple', get_values(purple))
 
-        trains = get_trains()
-
+        # Get the next arriving and departing trains using our library
+        trains = trains.get_schedule()
+        # and add them to the data for those platforms
         s_stats['trains'] = trains['S']
         red_stats['trains'] = trains['1-2-3']
 
+        # Construct the data structure for the JSON string.
         payload = {
                     'surface': street_stats,
                     'concourse': s_stats,
                     '1-2-3': red_stats,
                     '7': purple_stats
                   }
+        # Print it to stdout
         print(json.dumps(payload))
+        # Flush to prevent buffering
         stdout.flush()
+        # Pause for PAUSE time before repeating.
         time.sleep(PAUSE)
 
 
 if __name__ == '__main__':
+    # When we start up this file, initialize the global files...
     global_setup()
-    main()
+    # Then start the indefinite loop sending data to stdout
+    send()
